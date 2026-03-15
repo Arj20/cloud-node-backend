fc25ea8b5a46a150955a40c22999b2e0ec279079
Arihant Jain <arihant.jain67@gmail.com>
2026-03-15 20:08:09 +0530

    Update database connection configuration and add .env to .gitignore

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "arihantjain",
-  host: "localhost",
-  database: "tasksdb",
-  password: "",
-  port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
